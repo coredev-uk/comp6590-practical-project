23831688888d7945aa8b2e3b565bce3ea89c4e19
Paul Thompson <pt357@kent.ac.uk>
2025-05-02 19:26:42 +0100

    misc: add initial templating and classes

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("Hello via Bun!");
+import { readFileSync } from "fs";
+import { resolve } from "path";
+
+const TRAINING_DATA = resolve(__dirname, "training_riddles.json");
+
+class CompuRiddle {
+  constructor(refCorpus: string[]) {}
+
+  async run(batch = 5, topK = 2) {}
+}
+
+async function main() {
+  const refCorpus = JSON.parse(readFileSync(TRAINING_DATA, "utf-8"));
+
+  const system = new CompuRiddle(refCorpus);
+
+  const results = await system.run();
+
+  console.log(JSON.stringify(results, null, 2));
+}
+
+main();
